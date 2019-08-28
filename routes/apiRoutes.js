1a510e8ab04a6b8a2886672290d4b2ca4904ed28
unknown <baris.sahin.can@gmail.com>
2019-08-28 01:52:57 -0400

    email module added. picture size fixed

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,7 +1,35 @@
 var db = require("../models");
 var path = require("path");
+const nodemailer=require("nodemailer");
+require('dotenv').config()
 module.exports = function(app) {
     
+    app.post("/sendMail" , function(req, res) {
+      var message = req.body.message;
+      var senderEmail = req.body.senderEmail;
+      var senderName = req.body.senderName;
+      let transporter=nodemailer.createTransport({
+        service: "gmail",
+        auth : {
+            user: process.env.emailUser,
+            pass: process.env.emailPass,
+        }
+        });
+
+        let mailOption = {
+            from : "ucfhospital@gmail.com",
+            to : "baris.sahin.can@gmail.com",
+            subject: "New Message From Portfolio "  ,
+            text: message + "\n from " + senderEmail + "\n Sender Person Name : " + senderName
+        };
+
+        transporter.sendMail(mailOption, function(err, data){
+            if (err) console.log(err)
+            else res.json({msg:"Email sended"})
+        })
+    })
+
+
     app.post("/custom_projects", function(req, res) {
         // check if user is logged in
         console.log(req.body)
